6df94318d3015cef6fe4529c859dad0c2bd47bc4
Giulio Martinelli <this.error@protonmail.ch>
2024-03-19 16:31:34 -0300

    alter name variables

--- script.sh
+++ script.sh
@@ -2,16 +2,16 @@
 
 path="./build"
 
-path_paimon_docker="./paimon_docker"
-path_paimon_backend="./paimon_backend"
-path_paimon_frontend="./paimon_frontend"
+path_docker="./mikag_docker"
+path_backend="./mikag_backend"
+path_frontend="./mikag_frontend"
 
-git_paimon_docker="https://github.com/giuliomartinelli/paimon-docker-compose"
-git_paimon_backend="https://github.com/giuliomartinelli/paimon-backend-nestjs"
-git_paimon_frontend="https://github.com/giuliomartinelli/paimon-frontend-react"
+git_docker="https://github.com/giuliomartinelli/paimon-docker-compose"
+git_backend="https://github.com/giuliomartinelli/paimon-backend-nestjs"
+git_frontend="https://github.com/giuliomartinelli/paimon-frontend-react"
 
 cd $path
 
-git clone $git_paimon_docker $path_paimon_docker
-git clone $git_paimon_backend $path_paimon_backend
-#git clone $git_paimon_frontend $path_paimon_frontend
+git clone $git_docker $path_docker
+git clone $git_backend $path_backend
+git clone $git_frontend $path_frontend
